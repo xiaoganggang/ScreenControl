a0e7449d88d1141d1d9e4a586ca6dd530985c06d
xiaogangzai <1487067537@qq.com>
2017-05-23 20:27:09 +0800

    5月23日

--- app/src/main/java/gang/com/screencontrol/fragment/Login_Fragment_one.java
+++ app/src/main/java/gang/com/screencontrol/fragment/Login_Fragment_one.java
@@ -13,7 +13,6 @@ import android.widget.ImageView;
 
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.drafts.Draft_17;
-import org.java_websocket.handshake.ServerHandshake;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -24,11 +23,14 @@ import butterknife.OnClick;
 import butterknife.Unbinder;
 import gang.com.screencontrol.MainActivity;
 import gang.com.screencontrol.R;
-import gang.com.screencontrol.util.LogUtil;
+import gang.com.screencontrol.websocketo.ExampleClient;
+import gang.com.screencontrol.websocketo.Task;
+
 
 /**
  * Created by xiaogangzai on 2017/5/16.
  * http://www.chengxuyuans.com/Android/97798.html-----websocket简介
+ * http://www.open-open.com/lib/view/open1476778263175.html----两种方法实现websocket
  */
 
 public class Login_Fragment_one extends Fragment {
@@ -49,14 +51,16 @@ public class Login_Fragment_one extends Fragment {
     @BindView(R.id.button_login1)
     Button buttonLogin1;
     Unbinder unbinder;
-    private WebSocketClient client;
-    private String mUlWan = "";
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_login_one, container, false);
         unbinder = ButterKnife.bind(this, view);
+        Task task = new Task();
+        Thread td1 = new Thread(task, "td1");
+         //多个Thread也可以同时使用一个Runbale，
+        td1.start();
         return view;
     }
 
@@ -75,37 +79,47 @@ public class Login_Fragment_one extends Fragment {
             case R.id.image_login1_zidonglogin:
                 break;
             case R.id.button_login1:
+
+              /*  ExampleClient c = null;
                 try {
-                    client = new WebSocketClient(new URI(mUlWan), new Draft_17(), null, 3000) {
+                    c = new ExampleClient(new URI("wss://192.168.10.168:7681/test"), new Draft_17());
+                    c.connectBlocking();
+                    c.send("handshake");
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }*/
+                /*try {
+                    client = new WebSocketClient(URI.create(""), new Draft_17(), null, 10) {
                         //已连接服务器
                         @Override
                         public void onOpen(ServerHandshake handshakedata) {
+                            System.out.println("client onOpen");
 
                         }
 
                         //获取服务器信息
                         @Override
                         public void onMessage(String message) {
-
+                            System.out.println("client onMessage:" + message);
                         }
 
                         @Override
                         public void onClose(int code, String reason, boolean remote) {
-
+                            System.out.println("client onClose:" + code + " " + reason + " " + remote);
                         }
 
                         @Override
                         public void onError(Exception ex) {
-
+                            System.out.println("client onError:" + ex);
                         }
                     };
                     LogUtil.d("啦啦啦", "客户端建立连接connect()");
                     client.connect();
-                } catch (URISyntaxException e) {
+                }  catch (Exception e) {
                     e.printStackTrace();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                }*/
 
 
                 StartActivity(MainActivity.class);
--- app/src/main/java/gang/com/screencontrol/websocketo/Task.java
+++ app/src/main/java/gang/com/screencontrol/websocketo/Task.java
@@ -9,14 +9,16 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import gang.com.screencontrol.util.LogUtil;
+import gang.com.screencontrol.util.ToastUtil;
 
 /**
  * Created by xiaogangzai on 2017/5/22.
  * http://www.cnblogs.com/Conker/p/6508420.html
+ * http://blog.csdn.net/mad1989/article/details/38234759  Runnable的使用
  */
 
 public class Task implements Runnable {
-    private String mUlWan = "";
+    private String mUlWan = "wss://192.168.10.168:7681/test";
     private WebSocketClient mClient;
 
     @Override
@@ -27,10 +29,18 @@ public class Task implements Runnable {
 
                 return;
             }
-            mClient = new WebSocketClient(new URI(mUlWan), new Draft_17(), null, 3000) {
+            mClient = new WebSocketClient(new URI(mUlWan), new Draft_17(), null, 30* 1000) {
                 @Override
                 public void onOpen(ServerHandshake handshakedata) {
-
+                    LogUtil.d("啦啦啦开启", "开启");
+                   send("{\n" +
+                           "   \"body\" : {\n" +
+                           "      \"userName\" : \"Admin\",\n" +
+                           "      \"userPassword\" : \"admin\"\n" +
+                           "   },\n" +
+                           "   \"guid\" : \"M-0\",\n" +
+                           "   \"type\" : \"QUERYUSERLOGIN\"\n" +
+                           "}");
                 }
 
                 @Override
@@ -40,7 +50,7 @@ public class Task implements Runnable {
 
                 @Override
                 public void onClose(int code, String reason, boolean remote) {
-
+                    LogUtil.d("啦啦啦关闭", reason+remote);
                 }
 
                 @Override
@@ -56,4 +66,14 @@ public class Task implements Runnable {
             e.printStackTrace();
         }
     }
+
+    /**
+     * 发送用户名给服务器
+     */
+    private void sendUsername() {
+        String user = "啦啦啦";
+        if (user != null && user.length() != 0)
+            mClient.send(user);
+    }
+
 }
